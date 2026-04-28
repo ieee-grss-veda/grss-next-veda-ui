57bd4f838247b8e2ddf6fe1c684e3dd848d81187
rashmimanandhar <rashmi.manandhar7@gmail.com>
2026-04-28 17:43:39 -0400

    feat: integrate and theme osano cookie banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,15 +25,27 @@ export default function RootLayout({
             __html: `(function(){var t=localStorage.getItem('theme');if(t==='dark')document.documentElement.classList.add('dark');})();`,
           }}
         />
+
+        {/*  v ieee cookie banner v  */}
+        <script src='https://cmp.osano.com/AzyzptTmRlqVd2LRf/de836d52-6a96-4ecd-b0ed-945c5684d0a9/osano.js'></script>
+        <link
+          rel='stylesheet'
+          href='https://cookie-consent.ieee.org/ieee-cookie-banner.css'
+          type='text/css'
+        />
+        {/*  ^ ieee cookie banner ^  */}
       </head>
       <body>
         <ThemeProvider>
           <WebsiteTourProvider>
-            <div className='min-h-screen bg-background text-foreground' lang='en'>
+            <div
+              className='min-h-screen bg-background text-foreground'
+              lang='en'
+            >
               <Header />
               <div>{children}</div>
               <Footer />
-              <CookiesBanner />
+              {/* <CookiesBanner /> */}
             </div>
           </WebsiteTourProvider>
         </ThemeProvider>
